d4e7941941bdf5a06577aea609dd63c3f314e83b
Samuel <ngkwankiuu@gmail.com>
2024-04-27 23:31:57 -0700

    ACU 0.0.8 Pre-release (#10)
    ACU 0.0.8 Pre-release (#10)
    
    First ACU pre-release to main
    
    Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,146 @@
-# acu
-arch(arm)-config-util (acu) is a configuration utility for Arch Linux ARM (Aarch64) currently with main focus on the Radxa Rock 5 and RK3588.
+# ACU (A Configuration Utility for Arch Linux ARM)
+### Warning: ACU is still experimental. 
 
-# work-in-progress
-please check [dev branch](https://github.com/kwankiu/acu/tree/dev) for more infomation.
+![alt ACU Screenshot](https://i.imgur.com/DyaNIfv.png)
+
+## What is acu?
+
+- **Supported Platform**:  ACU is a community-built tool designed for managing configurations and packages on Arch Linux ARM (Aarch64). While primarily tailored for ARM architectures, it can be used on x86_64 platforms as well, although with limitations due to ARM/SBC-specific features. However, it is important to note that ACU is not fully supported on x86_64 platforms. Currently, ACU is optimized for the Rockchip RK3588 (Radxa Rock 5B), serving as a comprehensive utility for ARM Single Board Computers (SBCs).
+
+- **Configuration Utility**: ACU offers functionalities akin to popular configuration tools such as [armbian-config](https://github.com/armbian/config) or [raspi-config](https://www.raspberrypi.com/documentation/computers/configuration.html) or [rsetup](https://docs.radxa.com/en/radxa-os/rsetup/rsetup-tool) but for Arch Linux.
+
+- **Packages Management**: Serving as a package manager helper, ACU consolidates various package management tasks into a single, intuitive interface. It streamlines processes like selective package upgrades, downgrading to specific versions, installing packages from GitHub releases or URLs, as well as compiling and installing packages from source (PKGBUILD), including those from AUR or other Git repositories.
+
+- **Introducing ACU Apps**: ACU introduces an "App Store" like [pi-apps](https://github.com/Botspot/pi-apps), offering a curated collection of applications for Arch Linux ARM and ARM Single Board Computers (SBCs).
+
+- **System Infomation**: ACU provides system information akin to [Neofetch](https://github.com/dylanaraps/neofetch) but with enhanced ARM support. While standard system information tools may struggle with ARM SoC details, ACU ensures comprehensive reporting.
+
+- **Customization**: 
+ACU facilitates extensive customization through configuration files like `config.yaml`, `repo.yaml`, and `apps.yaml` (in `~/.acu/config`). These files empower users to tailor configurations, manage repositories, and creating your own or modifying app lists for ACU Apps. Moreover, users can load configurations from local paths or URLs using the `--loadconfig` option, allowing for seamless customization upstream or locally.
+
+## Sounds cool. How do I Install it?
+
+The configuration utility is pre-installed with Arch Linux installed and provided by this [archlinux-installer](https://github.com/kwankiu/archlinux-installer)
+
+### New Install :
+```
+bash <(curl -fsSL https://raw.githubusercontent.com/kwankiu/acu/main/acu) -u
+```
+(Notes: ACU automatically installs the latest version available using the -u command)
+
+## Tips
+
+### Updating repositories database
+Notes: To make sure the repositories and apps list is up-to-date, you should run `acu update` regularly, ACU will also sync pacman repositories database for you.
+
+```
+acu update
+```
+
+### Updating ACU
+To update ACU itself, when there is an update available, there will be an option shows up when you run `acu`. Alternatively, you may also update ACU manually using:
+```
+acu -u
+```
+To update to a specific version or channel:
+```
+acu --update=0.0.5-dev
+```
+
+### Removing ACU
+
+To uninstall ACU:
+```
+acu remove acu
+```
+If AGR is installed, you may want to remove AGR before removing ACU:
+```
+acu remove agr
+```
+
+### Install command
+To install a package:
+```
+acu install <package>
+```
+is same as
+```
+acu -S <package>
+```
+To install a package with a specific package manager / helper:
+```
+acu install <package> --usepm=<pm>
+# <pm> options: pacman, agr, git, ghrel.
+```
+To Install a new kernel:
+
+```
+acu install <package>
+```
+
+Install a package from source (git repository):
+1. Add a repository:
+```
+acu rem set <repo_name> <repo_url> <repo_type_(optional)> <gpg_key_(optional and for pacman only)>
+```
+2. Update / fetch the repository database:
+```
+acu update
+```
+3. Install a package:
+```
+acu install <package>
+```
+
+### Remove command
+```
+acu remove <package>
+```
+is same as
+```
+acu -R <package>
+```
+
+### Downgrade command
+```
+acu downgrade <package>
+```
+is same as
+```
+acu -D <package>
+```
+
+### Remote / Repositories command
+List ACU managed repositories
+```
+acu rem list
+```
+is same as
+```
+acu rem show
+```
+
+Fetch ACU managed repositories and list all packages
+```
+acu rem
+```
+is same as
+```
+acu rem fetch
+```
+
+More detailed documentation will be available on [wiki](https://github.com/kwankiu/acu/wiki)
+
+## Contributing
+
+Contributions to ACU are welcome! Whether you're interested in fixing bugs, adding features, or improving documentation, your contributions help enhance the utility for the community. Feel free to submit pull requests (PRs) with your changes.
+
+## Support
+
+For assistance, bug reports, or feature requests, please [open a Discussion](https://github.com/kwankiu/acu/discussions) on the ACU GitHub repository. Our community is here to help! Discussions are preferred for general inquiries, feature requests, or broader discussions. However, if you encounter a bug or have a specific issue, feel free to [open an issue](https://github.com/kwankiu/acu/issues).
+
+For real-time discussions about ACU, you can also join our Discord server [here](https://discord.gg/yY3F9b7hSK).
+
+## License
+
+ACU is licensed under the GPL v3 License, granting users the freedom to use, modify, and distribute the software.
---
+++ scripts/acu_parser.sh
@@ -0,0 +1,217 @@
+# /bin/bash
+
+# Define terminal color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;36m'
+DEBUG='\033[0;37m'
+NC='\033[0m' # No Color
+
+# Echo with colors
+colorecho() {
+    color="$1"
+    text="$2"
+    [ -n "$no_warning" ] && [ "$color" = "$YELLOW" ] || [ "$color" = "$DEBUG" ] && [ -z "$debug_log" ] || echo -e "${color}${text}${NC}"
+}
+
+################################################################
+debug_log=1
+# ACU File Pre-Parser
+# source: https://github.com/mrbaseman/pasrse_yaml.git
+# awk : process multi-line text (handle YAML pipe) (added a space for better syntax / readability)
+# sed : replace '-' with a space
+pre_parser() {
+    local indexfix=-1
+    local target_file=$1
+
+    # Handle URL
+    if [[ "$target_file" == *"://"* ]]; then
+        local temp_file=$(mktemp)
+        curl -s "$target_file" > "$temp_file"
+        target_file="$temp_file"
+    fi
+
+    # Detect awk flavor
+    if awk --version 2>&1 | grep -q "GNU Awk" ; then
+        # GNU Awk detected
+        indexfix=-1
+    elif awk -Wv 2>&1 | grep -q "mawk" ; then
+        # mawk detected
+        indexfix=0
+    fi
+
+    local s='[[:space:]]*' sm='[ \t]*' w='[a-zA-Z0-9_.]*' fs=${fs:-$(echo @|tr @ '\034')} i=${i:-  }
+
+    cat $target_file | \
+    awk -F$fs "{multi=0;
+        if(match(\$0,/$sm\|$sm$/)){multi=1; sub(/$sm\|$sm$/,\" \");}
+        if(match(\$0,/$sm>$sm$/)){multi=2; sub(/$sm>$sm$/,\" \");}
+        while(multi>0){
+            str=\$0; gsub(/^$sm/,\"\", str);
+            indent=index(\$0,str);
+            indentstr=substr(\$0, 0, indent+$indexfix) \"$i\";
+            obuf=\$0;
+            getline;
+            while(index(\$0,indentstr)){
+                obuf=obuf substr(\$0, length(indentstr)+1);
+                if (multi==1){obuf=obuf \"\\\\n\";}
+                if (multi==2){
+                    if(match(\$0,/^$sm$/))
+                        obuf=obuf \"\\\\n\";
+                        else obuf=obuf \" \";
+                }
+                getline;
+            }
+            sub(/$sm$/,\"\",obuf);
+            print obuf;
+            multi=0;
+            if(match(\$0,/$sm\|$sm$/)){multi=1; sub(/$sm\|$sm$/,\"\");}
+            if(match(\$0,/$sm>$sm$/)){multi=2; sub(/$sm>$sm$/,\"\");}
+        }
+    print}" | \
+    sed 's/^\( *\)-/\1 /'
+
+    # Remove the temporary file if it was created
+    if [[ -n $temp_file ]]; then
+        rm "$temp_file"
+    fi
+}
+
+# ACU File Parser
+# Limitation: this is not a full YAML parser, it is only designed to properly handle upto two sub-tree (subhead)
+# Limitation: an unnecessary space at line ending can potentially cause issues
+load_yaml() {
+
+    local target_file=$1
+    local suffix=$2
+    local output_format=$3 # format: list (default), append, non-list
+    local current_head=""
+    local current_subhead=""
+    local last_indent=""
+    local sub_item
+    local head_list=()
+    local occurrence
+    local found_exist
+    local i
+
+    # Pre-Parse the file or url and generate the formatted YAML output
+    target_file="$(pre_parser "$target_file")"
+
+    # Parse the data into variables format
+        while IFS= read -r line; do
+            # Ignore comments and empty lines
+            if ! [[ $line =~ ^[[:space:]]*($|#) ]]; then
+                # Count indentation
+                local indentation=$(expr "$line" : '^ *' / 2)
+                # Remove indent spaces
+                line="${line#"${line%%[![:space:]]*}"}"
+                # Handle double quotes
+                line="${line//\"/\\\"}"
+                if [ "$indentation" = 0 ]; then
+                    current_head="${line/:/}"
+                elif [[ "$line" == *":"* ]]; then
+                    if [ -z "$suffix" ]; then
+                        # Use current head as suffix
+                        current_value="${current_head}_"
+                    elif [ "$suffix" != " " ]; then
+                        # Use user specified suffix
+                        current_value="${suffix}_"
+                    else
+                        # Without suffix
+                        current_value=""
+                    fi
+                    if [ "$output_format" != "non-list" ]; then
+                        found_exist=0
+                        current_var=$(awk -F':' '{print $1}' <<< "${current_value}${line}")
+                        for item in "${head_list[@]}"; do
+                            if [ "$item" = "$current_var" ]; then
+                                found_exist=1
+                                break
+                            fi
+                        done
+                        if [ "$found_exist" -eq 0 ] && [ "$output_format" != "append" ]; then
+                            head_list+=("$current_var")
+                            current_value="${current_value}${line//: /=(\"}\")"
+                        else
+                            current_value="${current_value}${line//: /+=(\"}\")"
+                        fi
+                    else
+                        current_value="${current_value}${line//: /=\"}\""
+                    fi
+                    # Add variable closing after handling sub_items
+                    if [ "$sub_item" = 2 ]; then
+                        if [ "$output_format" != "non-list" ]; then
+                            echo "\")"
+                        else
+                            echo "\""
+                        fi
+                    fi
+                    if [[ "$current_value" != *":\"" ]] && [[ "$current_value" != *":\")" ]]; then
+                        echo "$current_value"
+                        sub_item=0
+                    else
+                        current_subhead="${line/:/}"
+                        sub_item=1
+                    fi
+                elif [ "$indentation" -gt "$last_indent" ] || [ "$indentation" == "$last_indent" ]; then
+                    if [ -z "$suffix" ]; then
+                        # Use current head as suffix
+                        current_value="${current_head}_${current_subhead}"
+                    elif [ "$suffix" != " " ]; then
+                        # Use user specified suffix
+                        current_value="${suffix}_${current_subhead}"
+                    else
+                        # Without suffix
+                        current_value="${current_subhead}"
+                    fi
+                    if [ "$output_format" != "non-list" ]; then
+                        found_exist=0
+                        for item in "${head_list[@]}"; do
+                            if [ "$item" = "$current_value" ]; then
+                                found_exist=1
+                                break
+                            fi
+                        done
+                        if [ "$found_exist" -eq 0 ] && [ "$output_format" != "append" ]; then
+                            head_list+=("$current_value")
+                            current_value="${current_value}=(\"${line}"
+                        else
+                            current_value="${current_value}+=(\"${line}"
+                        fi
+                    else
+                        current_value="${current_value}=\"${line}"
+                    fi
+                    if [ "$sub_item" = 1 ]; then
+                        echo -n "$current_value"
+                        sub_item=2
+                    else
+                        echo -n " $line"
+                    fi
+                else
+                    colorecho "$DEBUG" "Indent: $indentation, Unhandled Line: $line"
+                fi
+                last_indent=$indentation
+            fi
+        done <<< "$target_file"
+        # Fix sub_item that appears on the last line
+        # Add variable closing after handling sub_items
+        if [ "$sub_item" = 2 ]; then
+            if [ "$output_format" != "non-list" ]; then
+                    echo "\")"
+            else
+                    echo "\""
+            fi
+        fi
+}
+
+set_before=$( set -o posix; set | sed -e '/^_=*/d' )
+#eval $(load_yaml "config/config.yaml" " ")
+#load_yaml "example/apps.yaml" "" list
+#load_yaml "example/apps.yaml"
+#load_yaml "config/config.yaml"
+load_yaml "$@"
+
+#set_after=$( set -o posix; unset set_before; set | sed -e '/^_=/d' )
+#diff  <(echo "$set_before") <(echo "$set_after") | sed -e 's/^> //' -e '/^[[:digit:]].*/d'
+
